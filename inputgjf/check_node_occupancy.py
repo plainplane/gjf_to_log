42ccb20933c07eca381bd05b198416ac20fc425f
oli <olivesolaresknorr@gmail.com>
2023-02-02 09:26:30 -0500

    working on node occupancy control

--- inputgjf/check_node_occupancy.py
+++ inputgjf/check_node_occupancy.py
@@ -8,12 +8,19 @@ def queuedata():
     return str1
 
 
-def findopenstate_queuedata():
-    open_state_pattern = re.compile('somepattern')
+#
+def findopennode_queuedata():
+    #regex fgor any number but 32?
+    #make counter to count node # when checking
+    for i in range(1-14):
+        open_state_pattern = re.compile('all.q@n1\wBIP\w32^*/32')
     match_data = open_state_pattern.start(queuedata())
     ##catch no match here and return to another function
+    ##queue : line under all.q@n# when job running, then dashes and so on
+    #states 1 at end of job line, au at end of nodeline, ID at start, # of nodes used 0/32 r?, load_avg 0.00
+    #find q@n# ---> 0/32 -> save node num --> return tup node # and TRUE/FALSE
     return match_data
-
+##one function to find open computer n# one function to find node #/32?
 
 def determine node number():
     list of possible pos indexed in order of node number
