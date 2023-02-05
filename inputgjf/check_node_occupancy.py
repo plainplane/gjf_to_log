5ee9d7d080bcdeabe12354d47c3a7d017c52d1b0
oli <olivesolaresknorr@gmail.com>
2023-02-05 14:39:05 -0500

    working on node occupancy control nodelist_q done

--- inputgjf/check_node_occupancy.py
+++ inputgjf/check_node_occupancy.py
@@ -1,30 +1,42 @@
 import subprocess
 import re
+import time
 # call bash; variable=bash string output of queue
 
 
-def queuedata():
+def qdata():
     str1 = subprocess.run(["qs"])
     return str1
 
 
 #
-def findopennode_queuedata(node_range_min,node_range_max):
-    #regex fgor any number but 32?
-    count = 0
-    open_nodes= []
+def nodeslist_qdata():
+    # node_to_jobnum_pattern= re.compile(r'all.q@n'+str(i)+' *BIP *[1-9]*[1-9]/32(.*)\n\s*[0-9]{5}')
+    # node_to_jobnum_string = node_to_jobnum_pattern.group()
+    # node_to_jobnum_substrings_bywhitespace = node_to_jobnum_string.split()
+    # job_num = int(node_to_jobnum_substrings_bywhitespace[[len(node_to_jobnum_substrings_bywhitespace)]])
+    node_num = 0
+    individual_node_data = []
     for i in range(1,14):
-        count += 1
-        open_state_pattern = re.compile(r'all.q@n'+str(i)+'\wBIP\w[0-9]/32')
-        if re.match(open_state_pattern,queuedata()):
-            open_nodes += count
-        if re.match()
+        time.sleep(30)
+        node_num += 1
+        open_node_pattern = re.compile(r'all.q@n'+str(i)+' *BIP *[0-9]|[12][0-9]|[3][01]|/32')
+        closed_node_pattern = re.compile(r'all.q@n'+str(i)+' *BIP *32/32')
+
+        if re.match(open_node_pattern, queuedata()):
+            is_open = True
+        if re.match(closed_node_pattern, queuedata()):
+            is_open = False
+
+        node_to_endofnodesdata_pattern = re.compile(r'all.q@n' + str(i) + ' *BIP *[1-9]*[1-9]/32(.*)---')
+        node_datum = node_to_endofnodesdata_pattern.group(queuedata())
 
 
-    ##catch no match here and return to another function
+        individual_node_data += [(node_num,is_open,node_datum)]
+    return individual_node_data
 
-    #find q@n# ---> 0/32 -> save node num --> return tup node # and TRUE/FALSE
-##one function to find open computer n# one function to find node #/32?
+def nodejobs_qdata(nodeslist_queuedata(),):
+
 
 def determine node number():
     list of possible pos indexed in order of node number
